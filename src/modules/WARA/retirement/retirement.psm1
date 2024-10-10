bc3273788a589937ffbeee16143b54b7a0885afa
Takeshi Katano <takatano@outlook.com>
2024-10-11 00:10:32 +0900

    Add comment help

--- src/modules/WARA/retirement/retirement.psm1
+++ src/modules/WARA/retirement/retirement.psm1
@@ -1,3 +1,40 @@
+<#
+.SYNOPSIS
+    Retrieves active retirement health advisory events.
+
+.DESCRIPTION
+    This module contains functions related to the capturing and collecting to active retirement health advisory events.
+    It includes the following functions:
+    - Get-WAFResourceRetirement
+    - Invoke-AzureRestApi
+    - Get-AzureRestMethodUriPath
+    - New-WAFResourceRetirementObject
+
+.EXAMPLE
+    $retirementObjects = Get-WAFResourceRetirement -SubscriptionId '11111111-1111-1111-1111-111111111111'
+
+.NOTES
+    Author: Takeshi Katano
+    Date: 2024-10-02
+#>
+
+<#
+.SYNOPSIS
+    Retrieves active retirement health advisory events based on the specified subscription ID.
+
+.DESCRIPTION
+    The Get-WAFResourceRetirement function takes a subscription ID and retrieves active retirement health advisory events.
+
+.PARAMETER SubscriptionId
+    A subscription ID to retrieves active retirement health advisory events.
+
+.EXAMPLE
+    $retirementObjects = Get-WAFResourceRetirement -SubscriptionId '11111111-1111-1111-1111-111111111111'
+
+.NOTES
+    Author: Takeshi Katano
+    Date: 2024-10-02
+#>
 function Get-WAFResourceRetirement {
     [CmdletBinding()]
     param (
@@ -43,7 +80,49 @@ function Get-WAFResourceRetirement {
     return $retirementObjects
 }
 
-# NOTE: This function should be placed in the utils.psm1, or implement a similar function in the utils.psm1.
+<#
+.SYNOPSIS
+    Invokes an Azure REST API then returns the response.
+
+.DESCRIPTION
+    The Invoke-AzureRestApi function invokes an Azure REST API with the specified parameters then return the response.
+
+.PARAMETER Method
+    The HTTP method to invoke the Azure REST API.
+
+.PARAMETER SubscriptionId
+    The subscription ID that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER ResourceGroupName
+    The resource group name that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER ResourceProviderName
+    The resource provider name that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER ResourceType
+    The resource type that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER Name
+    The resource name that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER ApiVersion
+    The Azure REST API version that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER QueryString
+    The query string that constitutes the URI for invoke the Azure REST API.
+
+.PARAMETER RequestBody
+    The request body for invoke the Azure REST API.
+
+.EXAMPLE
+    $response = Invoke-AzureRestApi -Method 'GET' -SubscriptionId '11111111-1111-1111-1111-111111111111' -ResourceProviderName 'Microsoft.ResourceHealth' -ResourceType 'events' -ApiVersion '2024-02-01' -QueryString 'queryStartTime=2024-10-02T00:00:00'
+
+.NOTES
+    Author: Takeshi Katano
+    Date: 2024-10-02
+
+    This function should be placed in a common module such as a utility module because this is common feature across modules.
+#>
 function Invoke-AzureRestApi {
     [CmdletBinding()]
     param (
@@ -103,7 +182,45 @@ function Invoke-AzureRestApi {
     return Invoke-AzRestMethod @cmdletParams
 }
 
-# NOTE: This function should be placed in the utils.psm1.
+<#
+.SYNOPSIS
+    Retrieves the path of the Azure REST API URI.
+
+.DESCRIPTION
+    The Get-AzureRestMethodUriPath function retrieves the formatted path of the Azure REST API URI based on the specified URI parts as parameters.
+    The path represents the Azure REST API URI without the protocol (e.g. https), host (e.g. management.azure.com). For example,
+    /subscriptions/11111111-1111-1111-1111-111111111111/resourceGroups/rg1/providers/Microsoft.Storage/storageAccounts/stsample1234?api-version=2024-01-01
+
+.PARAMETER SubscriptionId
+    The subscription ID that constitutes the path of Azure REST API URI.
+
+.PARAMETER ResourceGroupName
+    The resource group name that constitutes the path of Azure REST API URI.
+
+.PARAMETER ResourceProviderName
+    The resource provider name that constitutes the path of Azure REST API URI.
+
+.PARAMETER ResourceType
+    The resource type that constitutes the path of Azure REST API URI.
+
+.PARAMETER Name
+    The resource name that constitutes the path of Azure REST API URI.
+
+.PARAMETER ApiVersion
+    The Azure REST API version that constitutes the path of Azure REST API URI.
+
+.PARAMETER QueryString
+    The query string that constitutes the path of Azure REST API URI.
+
+.EXAMPLE
+    $path = Get-AzureRestMethodUriPath -SubscriptionId '11111111-1111-1111-1111-111111111111' -ResourceGroupName 'rg1' -ResourceProviderName 'Microsoft.Storage' -ResourceType 'storageAccounts' -Name 'stsample1234' -ApiVersion '2024-01-01'
+
+.NOTES
+    Author: Takeshi Katano
+    Date: 2024-10-02
+
+    This function should be placed in a common module such as a utility module because this is common feature across modules.
+#>
 function Get-AzureRestMethodUriPath {
     [CmdletBinding()]
     param (
@@ -147,6 +264,56 @@ function Get-AzureRestMethodUriPath {
     return $path
 }
 
+<#
+.SYNOPSIS
+    Creates a retirement object.
+
+.DESCRIPTION
+    The New-WAFResourceRetirementObject function creates a retirement object based on the specified parameters.
+
+.PARAMETER SubscriptionId
+    The subscription ID of the retirement event.
+
+.PARAMETER TrackingId
+    The tracking ID of the retirement event.
+
+.PARAMETER Status
+    The status of the retirement event.
+
+.PARAMETER LastUpdateTime
+    The last update time of the retirement event.
+
+.PARAMETER StartTime
+    The impact start time of the retirement event.
+
+.PARAMETER EndTime
+    The impact mitigation time of the retirement event.
+
+.PARAMETER Level
+    The level of the retirement event.
+
+.PARAMETER Title
+    The title of the retirement event.
+
+.PARAMETER Summary
+    The summary of the retirement event.
+
+.PARAMETER Header
+    The header of the retirement event.
+
+.PARAMETER ImpactedService
+    The impacted services of the retirement event.
+
+.PARAMETER Description
+    The description of the retirement event.
+
+.EXAMPLE
+    retirementObject = New-WAFResourceRetirementObject -SubscriptionId $subscriptionId -TrackingId $trackingId -Status $status -LastUpdateTime $lastUpdateTime -Starttime $startTime -Endtime $endTime -Level $level -Title $title -Summary $summary -Header $header -ImpactedService $impactedServices -Description $description
+
+.NOTES
+    Author: Takeshi Katano
+    Date: 2024-10-02
+#>
 function New-WAFResourceRetirementObject {
     [CmdletBinding()]
     param (
