cb9c612af28df401254e1edc7cb1824bef84f186
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-10-14 11:45:58 -0600

    fix: test pipelines

--- .github/scripts/RunPesterTests.ps1
+++ .github/scripts/RunPesterTests.ps1
@@ -51,4 +51,17 @@ foreach ($moduleDir in $moduleDirectories) {
     }
 }
 
-If($($coveragePercent + $resultOfRun + $passedCount + $failedCount).contains("Failed")){Write-host "Failed";Exit 1}else{Write-host "Passed"}
+If($($coveragePercent + $resultOfRun + $passedCount + $failedCount).contains("Failed"))
+{
+    Write-host "Failed"
+    
+    if($env:GITHUB_WORKSPACE){
+        echo "ERROR_DETECTED=true" >> $GITHUB_ENV
+        Exit 1
+    }
+    
+}
+else
+{
+    Write-host "Passed"
+}
