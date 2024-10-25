a6077db927a5767d97edb0224f48186428cef44a
Casey Watson <cawatson@microsoft.com>
2024-10-25 13:26:01 -0500

    Added function to read runbook file.

--- src/modules/wara/runbook/runbook.psm1
+++ src/modules/wara/runbook/runbook.psm1
@@ -1,57 +1,81 @@
 <#
 .SYNOPSIS
-WARA Runbook module
+Reads a runbook
 
 .DESCRIPTION
-Enables developers to consume WARA runbook files
+The Read-WAFRunbook function converts a WAF runbook file to a corresponding hashtable
 #>
+function Read-WAFRunbook {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$RunbookPath
+  )
 
-function Read-Runbook {
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$RunbookPath
-    )
+  # Check that the file actually exists
+  if (-not $(Test-Path $RunbookPath -PathType Leaf)) {
+    throw "Runbook file not found: [$RunbookPath]"
+  }
 
-    # First, check to make sure the runbook actually exists...
-    if (!(Test-Path $RunbookPath -PathType Leaf)) {
+  $runbook = @{
+    Parameters     = @{}
+    Selectors      = @{}
+    Checks         = @{}
+    QueryOverrides = @()
+  }
 
-        # If not, fail early.
-        Write-Error "[-RunbookPath]: No runbook found at [$RunbookPath]."
-        $null
+  # Read the runbook JSON
+  $fileJson = Get-Content -Raw $RunbookPath | ConvertFrom-Json -AsHashtable
 
-    } else {
 
-        # If so, let's read this runbook!
-        $runbook = @{
-            Parameters = @{}
-            Selectors = @{}
-            Checks = @{}
-            QueryOverrides = @()
-        }
-        
-        # Read the runbook JSON
-        $runbookJson = Get-Content -Raw $RunbookPath | ConvertFrom-Json
 
-        # Try to load parameters
-        $runbookJson.parameters.PSObject.Properties | ForEach-Object {
-          $runbook.Parameters[$_.Name] = $_.Value
-        }
-
-        # Try to load selectors
-        $runbookJson.selectors.PSObject.Properties | ForEach-Object {
-          $runbook.Selectors[$_.Name.ToLower()] = $_.Value
-        }
-
-        # Try to load checks
-        $runbookJson.checks.PSObject.Properties | ForEach-Object {
-          $runbook.Checks[$_.Name.ToLower()] = $_.Value
-        }
-
-        # Try to load query overrides
-        $runbookJson.query_overrides | ForEach-Object {
-          $runbook.QueryOverrides += [string]$_
-        }
-
-        return [pscustomobject]$runbook
+  if ($fileJson.parameters -and $fileJson.parameters.Count -gt 0) {
+    # Read parameters
+    foreach ($parameterKey in $fileJson.parameters.Keys) {
+      $runbook.Parameters[$parameterKey] = $fileJson.parameters[$parameterKey]
     }
+  }
+  else {
+    # It's odd that there's no parameters
+    Write-Warning "Runbook [$RunbookPath] defines no [parameters]."
+  }
+
+  if ($fileJson.selectors) {
+    # Read selectors
+    foreach ($selectorKey in $fileJson.selectors.Keys) {
+      $runbook.Selectors[$selectorKey] = $fileJson.selectors[$selectorKey]
+    }
+  }
+  else {
+    # Checks are required and checks require selectors.
+    # Therefore, selectors are required.
+    throw "Runbook [$RunbookPath] defines no [selectors]. [selectors] are required."
+  }
+
+        
+  # Read the runbook JSON
+  $runbookJson = Get-Content -Raw $RunbookPath | ConvertFrom-Json
+
+  # Try to load parameters
+  $runbookJson.parameters.PSObject.Properties | ForEach-Object {
+    $runbook.Parameters[$_.Name] = $_.Value
+  }
+
+  # Try to load selectors
+  $runbookJson.selectors.PSObject.Properties | ForEach-Object {
+    $runbook.Selectors[$_.Name.ToLower()] = $_.Value
+  }
+
+  # Try to load checks
+  $runbookJson.checks.PSObject.Properties | ForEach-Object {
+    $runbook.Checks[$_.Name.ToLower()] = $_.Value
+  }
+
+  # Try to load query overrides
+  $runbookJson.query_overrides | ForEach-Object {
+    $runbook.QueryOverrides += [string]$_
+  }
+
+  return [pscustomobject]$runbook
 }
+}
+#>
