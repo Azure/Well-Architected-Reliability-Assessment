3c6a1acdbc6d1878d8d59f49b0fdc947d47347d3
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-11-13 15:00:07 -0700

    update: advisor testdata and tests

--- src/tests/advisor/advisor.tests.ps1
+++ src/tests/advisor/advisor.tests.ps1
@@ -1,6 +1,6 @@
 BeforeAll {
     $modulePath = "$PSScriptRoot/../../modules/wara/advisor/advisor.psm1"
-    $testDataPath = "$PSScriptRoot/../data/newAdvisorData.json"
+    $testDataPath = "$PSScriptRoot/../data/bigAdvisorTestData.json"
     Import-Module -Name $modulePath -Force
         $objectlist = get-content $testDataPath -Raw | ConvertFrom-Json -depth 10
 }
@@ -9,17 +9,17 @@ Describe "Build-WAFAdvisorObject" {
     Context "When given a raw list of advisories" {
         It "Should return the processed list as object" {
             $result = Build-WAFAdvisorObject -AdvQueryResult $objectlist
-            $result | Should -HaveCount 2
-            $result.recommendationId    | Should -Contain 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA'
-            $result.type                | Should -Contain 'MICROSOFT.CONTAINERSERVICE/MANAGEDCLUSTERS'
-            $result.name                | Should -Contain 'akscluster01'
-            $result.id                  | Should -Contain '/subscriptions/11111111-1111-1111-1111-111111111111/resourcegroups/resourcegroup01/providers/microsoft.containerService/managedclusters/akscluster01'
-            $result.subscriptionId      | Should -Contain '11111111-1111-1111-1111-111111111111'
-            $result.resourceGroup       | Should -Contain 'resourcegroup01'
-            $result.location            | Should -Contain 'centralus'
+            $result | Should -HaveCount 448
+            $result.recommendationId    | Should -Contain '242639fd-cd73-4be2-8f55-70478db8d1a5'
+            $result.type                | Should -Contain 'microsoft.subscriptions/subscriptions'
+            $result.name                | Should -Contain '5555555-5555-5555-5555-555555555555'
+            $result.id                  | Should -Contain '/subscriptions/5555555-5555-5555-5555-555555555555'
+            $result.subscriptionId      | Should -Contain '5555555-5555-5555-5555-555555555555'
+            $result.resourceGroup       | Should -Contain 'N/A'
+            $result.location            | Should -Contain 'global'
             $result.category            | Should -Contain 'HighAvailability'
             $result.impact              | Should -Contain 'High'
-            $result.description         | Should -Contain 'Enable Autoscaling for your system node pools'
+            $result.description         | Should -Contain 'Create an Azure Service Health alert'
         }
     }
 }
