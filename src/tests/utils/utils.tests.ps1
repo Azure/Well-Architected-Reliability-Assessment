d336981fe874c5220f18b8d887769f6f0c8e5a44
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-11-19 10:45:47 -0700

    add: configfile tests

--- src/tests/utils/utils.tests.ps1
+++ src/tests/utils/utils.tests.ps1
@@ -1,7 +1,7 @@
 BeforeAll {
     $modulePath = "$PSScriptRoot/../../modules/wara/utils/utils.psd1"
     Import-Module -Name $modulePath -Force
-    Import-Module -Name 'Az.Accounts' -Force
+    Import-Module -Name 'Az.Accounts'
 }
 
 Describe 'Get-AzureRestMethodUriPath' {
@@ -150,6 +150,87 @@ Describe 'Invoke-AzureRestApi' {
 
             Should -InvokeVerifiable
             $result.Content | Should -BeExactly $expected
+        }
+    }
+}
+
+Describe Import-WAFConfigFileData {
+    BeforeEach {
+        $TestConfigFile1 = "$PSScriptRoot/../data/utils/testconfig1.txt"
+
+        $result = Import-WAFConfigFileData $TestConfigFile1
+
+
+        $expectedTenantId = "12121212-1212-1212-1212-121212121212"
+        $expectedSubscriptionIds = @(
+            "/subscriptions/0000000-0000-0000-0000-000000000000"
+        )
+        $expectedResourceGroups = @(
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-01",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-02",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-03",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-04",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-05",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-06",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-07",
+            "/subscriptions/1111111-1111-1111-1111-111111111111/resourceGroups/RG-08"
+        )
+        $expectedTags = @(
+            "env||environment=~preprod",
+            "app||application!~app1||app2"
+        )
+    }
+
+    Context 'Import a WAF config file with valid and invalid data' {
+        It 'Should return the correct content of the WAF config file' {
+            # Call the function with the test configuration file
+            
+            # Validate the results
+            $result.tenantid | Should -BeExactly $expectedTenantId
+            $result.bad1 | Should -BeNullOrEmpty
+            $result.subscriptionIds | Should -BeExactly $expectedSubscriptionIds
+            $result.resourcegroups | Should -Be $expectedResourceGroups
+            $result.tags | Should -BeExactly $expectedTags
+        }
+    }  
+}
+Describe 'Connect-WAFAzure' {
+    Mock -CommandName Get-AzContext -MockWith { $null }
+    Mock -CommandName Connect-AzAccount
+
+    Context 'When TenantID is provided' {
+        It 'Should call Connect-AzAccount with the correct parameters' {
+            $tenantId = [Guid]::NewGuid()
+            $azureEnvironment = 'AzureCloud'
+
+            Connect-WAFAzure -TenantID $tenantId -AzureEnvironment $azureEnvironment
+
+            # Verify that Connect-AzAccount was called with the correct parameters
+            Assert-MockCalled -CommandName Connect-AzAccount -Exactly 1 -Scope It -ParameterFilter {
+                $Tenant -eq $tenantId -and $Environment -eq $azureEnvironment
+            }
+        }
+
+        It 'Should use the default AzureEnvironment if not specified' {
+            $tenantId = [Guid]::NewGuid()
+
+            Connect-WAFAzure -TenantID $tenantId
+
+            # Verify that Connect-AzAccount was called with the default AzureEnvironment
+            Assert-MockCalled -CommandName Connect-AzAccount -Exactly 1 -Scope It -ParameterFilter {
+                $Tenant -eq $tenantId -and $Environment -eq 'AzureCloud'
+            }
+        }
+
+        It 'Should not call Connect-AzAccount if Get-AzContext returns a context' {
+            Mock -CommandName Get-AzContext -MockWith { @{ Context = 'SomeContext' } }
+
+            $tenantId = [Guid]::NewGuid()
+
+            Connect-WAFAzure -TenantID $tenantId
+
+            # Verify that Connect-AzAccount was not called
+            Assert-MockCalled -CommandName Connect-AzAccount -Exactly 0 -Scope It
         }
     }
 }
