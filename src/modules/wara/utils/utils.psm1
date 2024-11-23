a514b4820f7b228bc28c389f1d90b00c1c48c2fe
Takeshi Katano <takatano@outlook.com>
2024-11-23 20:13:54 +0900

    Fix indentation

--- src/modules/wara/utils/utils.psm1
+++ src/modules/wara/utils/utils.psm1
@@ -27,99 +27,99 @@ Function Invoke-WAFQuery {
 
 <#
 .SYNOPSIS
-    Invokes an Azure REST API then returns the response.
+  Invokes an Azure REST API then returns the response.
 
 .DESCRIPTION
-    The Invoke-AzureRestApi function invokes an Azure REST API with the specified parameters then return the response.
+  The Invoke-AzureRestApi function invokes an Azure REST API with the specified parameters then return the response.
 
 .PARAMETER Method
-    The HTTP method to invoke the Azure REST API. The accepted values are GET, POST, PUT, PATCH, and DELETE.
+  The HTTP method to invoke the Azure REST API. The accepted values are GET, POST, PUT, PATCH, and DELETE.
 
 .PARAMETER SubscriptionId
-    The subscription ID that constitutes the URI for invoke the Azure REST API.
+  The subscription ID that constitutes the URI for invoke the Azure REST API.
 
 .PARAMETER ResourceGroupName
-    The resource group name that constitutes the URI for invoke the Azure REST API.
+  The resource group name that constitutes the URI for invoke the Azure REST API.
 
 .PARAMETER ResourceProviderName
-    The resource provider name that constitutes the URI for invoke the Azure REST API. It's usually as the XXXX.XXXX format.
+  The resource provider name that constitutes the URI for invoke the Azure REST API. It's usually as the XXXX.XXXX format.
 
 .PARAMETER ResourceType
-    The resource type that constitutes the URI for invoke the Azure REST API.
+  The resource type that constitutes the URI for invoke the Azure REST API.
 
 .PARAMETER Name
-    The resource name that constitutes the URI for invoke the Azure REST API.
+  The resource name that constitutes the URI for invoke the Azure REST API.
 
 .PARAMETER ApiVersion
-    The Azure REST API version that constitutes the URI for invoke the Azure REST API. It's usually as the yyyy-mm-dd format.
+  The Azure REST API version that constitutes the URI for invoke the Azure REST API. It's usually as the yyyy-mm-dd format.
 
 .PARAMETER QueryString
-    The query string that constitutes the URI for invoke the Azure REST API.
+  The query string that constitutes the URI for invoke the Azure REST API.
 
 .PARAMETER RequestBody
-    The request body for invoke the Azure REST API.
+  The request body for invoke the Azure REST API.
 
 .OUTPUTS
-    Returns a REST API response as the PSHttpResponse.
+  Returns a REST API response as the PSHttpResponse.
 
 .EXAMPLE
-    PS> $response = Invoke-AzureRestApi -Method 'GET' -SubscriptionId '11111111-1111-1111-1111-111111111111' -ResourceProviderName 'Microsoft.ResourceHealth' -ResourceType 'events' -ApiVersion '2024-02-01' -QueryString 'queryStartTime=2024-10-02T00:00:00'
+  PS> $response = Invoke-AzureRestApi -Method 'GET' -SubscriptionId '11111111-1111-1111-1111-111111111111' -ResourceProviderName 'Microsoft.ResourceHealth' -ResourceType 'events' -ApiVersion '2024-02-01' -QueryString 'queryStartTime=2024-10-02T00:00:00'
 
 .NOTES
-    Author: Takeshi Katano
-    Date: 2024-10-23
+  Author: Takeshi Katano
+  Date: 2024-10-23
 
-    This function requires the Az.Accounts module to be installed and imported.
+  This function requires the Az.Accounts module to be installed and imported.
 #>
 function Invoke-AzureRestApi {
   [CmdletBinding()]
   [OutputType([Microsoft.Azure.Commands.Profile.Models.PSHttpResponse])]
   param (
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [ValidateSet('GET', 'POST', 'PUT', 'PATCH', 'DELETE')]
-      [string] $Method,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [ValidateSet('GET', 'POST', 'PUT', 'PATCH', 'DELETE')]
+    [string] $Method,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [ValidatePattern('^[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}$')]
-      [string] $SubscriptionId,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [ValidatePattern('^[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}$')]
+    [string] $SubscriptionId,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [ValidateLength(1, 90)]
-      [string] $ResourceGroupName,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [ValidateLength(1, 90)]
+    [string] $ResourceGroupName,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [string] $ResourceProviderName,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [string] $ResourceProviderName,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [string] $ResourceType,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [string] $ResourceType,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [string] $Name,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [string] $Name,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [ValidatePattern('^[0-9]{4}(-[0-9]{2}){2}$')]
-      [string] $ApiVersion,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [ValidatePattern('^[0-9]{4}(-[0-9]{2}){2}$')]
+    [string] $ApiVersion,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $false)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $false)]
-      [string] $QueryString,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $false)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $false)]
+    [string] $QueryString,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $false)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $false)]
-      [string] $RequestBody
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $false)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $false)]
+    [string] $RequestBody
   )
 
   # Built the Azure REST API URI path.
   $cmdletParams = @{
-      SubscriptionId       = $SubscriptionId
-      ResourceProviderName = $ResourceProviderName
-      ResourceType         = $ResourceType
-      ApiVersion           = $ApiVersion
+    SubscriptionId       = $SubscriptionId
+    ResourceProviderName = $ResourceProviderName
+    ResourceType         = $ResourceType
+    ApiVersion           = $ApiVersion
   }
   if ($PSBoundParameters.ContainsKey('ResourceGroupName')) { $cmdletParams.ResourceGroupName = $ResourceGroupName }
   if ($PSBoundParameters.ContainsKey('Name')) { $cmdletParams.Name = $Name}
@@ -128,8 +128,8 @@ function Invoke-AzureRestApi {
 
   # Invoke the Azure REST API using the URI path.
   $cmdletParams = @{
-      Method = $Method
-      Path   = $path
+    Method = $Method
+    Path   = $path
   }
   if ($PSBoundParameters.ContainsKey('RequestBody')) { $cmdletParams.Payload = $RequestBody }
   return Invoke-AzRestMethod @cmdletParams
@@ -179,45 +179,45 @@ function Get-AzureRestMethodUriPath {
   [CmdletBinding()]
   [OutputType([string])]
   param (
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [ValidatePattern('^[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}$')]
-      [string] $SubscriptionId,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [ValidatePattern('^[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}$')]
+    [string] $SubscriptionId,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [ValidateLength(1, 90)]
-      [string] $ResourceGroupName,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [ValidateLength(1, 90)]
+    [string] $ResourceGroupName,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [string] $ResourceProviderName,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [string] $ResourceProviderName,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [string] $ResourceType,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [string] $ResourceType,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [string] $Name,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [string] $Name,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
-      [ValidatePattern('^[0-9]{4}(-[0-9]{2}){2}$')]
-      [string] $ApiVersion,
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $true)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $true)]
+    [ValidatePattern('^[0-9]{4}(-[0-9]{2}){2}$')]
+    [string] $ApiVersion,
 
-      [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $false)]
-      [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $false)]
-      [string] $QueryString
+    [Parameter(ParameterSetName = 'WithResourceGroup', Mandatory = $false)]
+    [Parameter(ParameterSetName = 'WithoutResourceGroup', Mandatory = $false)]
+    [string] $QueryString
   )
 
   $additionalQueryString = if ($PSBoundParameters.ContainsKey('QueryString')) { '&' + $QueryString } else { '' }
   $path = if ($PSCmdlet.ParameterSetName -eq 'WithResourceGroup') {
-      '/subscriptions/{0}/resourcegroups/{1}/providers/{2}/{3}/{4}?api-version={5}{6}' -f $SubscriptionId, $ResourceGroupName, $ResourceProviderName, $ResourceType, $Name, $ApiVersion, $additionalQueryString
+    '/subscriptions/{0}/resourcegroups/{1}/providers/{2}/{3}/{4}?api-version={5}{6}' -f $SubscriptionId, $ResourceGroupName, $ResourceProviderName, $ResourceType, $Name, $ApiVersion, $additionalQueryString
   }
   elseif ($PSCmdlet.ParameterSetName -eq 'WithoutResourceGroup') {
-      '/subscriptions/{0}/providers/{1}/{2}?api-version={3}{4}' -f $SubscriptionId, $ResourceProviderName, $ResourceType, $ApiVersion, $additionalQueryString
+    '/subscriptions/{0}/providers/{1}/{2}?api-version={3}{4}' -f $SubscriptionId, $ResourceProviderName, $ResourceType, $ApiVersion, $additionalQueryString
   }
   else {
-      throw 'Invalid ParameterSetName'
+    throw 'Invalid ParameterSetName'
   }
   return $path
 }
