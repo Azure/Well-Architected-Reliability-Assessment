c273c4bc0b3ed0a92dcdba7e12eba166d64c0193
Takeshi Katano <takatano@outlook.com>
2024-11-23 19:55:54 +0900

    Use util's function to validate GUID

--- src/modules/wara/support/support.psm1
+++ src/modules/wara/support/support.psm1
@@ -45,7 +45,7 @@ function Get-WAFSupportTicket {
     [OutputType([PSCustomObject[]])]
     param (
         [Parameter(Mandatory = $true)]
-        [ValidatePattern('^[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}$')]
+        [ValidateScript({ Test-WAFIsGuid -StringGuid $_ })]
         [string[]] $SubscriptionIds
     )
 
