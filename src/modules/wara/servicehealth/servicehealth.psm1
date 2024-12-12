7a64ce132d6f866f7b17226c8d104ac65767076f
Kyle Poineal <38540295+kpoineal@users.noreply.github.com>
2024-12-12 15:42:51 -0700

    Update service health docs

--- src/modules/wara/servicehealth/servicehealth.psm1
+++ src/modules/wara/servicehealth/servicehealth.psm1
@@ -1,5 +1,33 @@
 using module ../utils/utils.psd1
+<#
+.SYNOPSIS
+    Retrieves service health alerts from specified Azure subscriptions.
 
+.DESCRIPTION
+    The `Get-WAFServiceHealth` function queries Azure Resource Graph to retrieve service health alerts from the provided subscription IDs. It searches for activity log alerts related to 'ServiceHealth' and joins them with subscription information.
+
+.PARAMETER SubscriptionIds
+    An array of Azure subscription IDs for which to retrieve service health alerts.
+
+.INPUTS
+    None. You cannot pipe objects to this function.
+
+.OUTPUTS
+    System.Object[]. Returns an array of service health alert objects.
+
+.EXAMPLE
+    # Retrieve service health alerts for multiple subscriptions
+    $subscriptions = @('59f6f1ab-6d68-4c90-b4e5-ad2d71cefc57', 'abcd1234-5678-90ab-cdef-1234567890ab')
+    $serviceHealthAlerts = Get-WAFServiceHealth -SubscriptionIds $subscriptions
+
+.EXAMPLE
+    # Retrieve service health alerts for a single subscription
+    $serviceHealthAlerts = Get-WAFServiceHealth -SubscriptionIds '59f6f1ab-6d68-4c90-b4e5-ad2d71cefc57'
+
+.NOTES
+    Author: Kyle Poineal
+    Date: 2024-12-12
+#>
 function Get-WAFServiceHealth {
     [CmdletBinding()]
     param (
@@ -25,6 +53,31 @@ function Get-WAFServiceHealth {
     return $AllServiceHealth
 }
 
+<#
+.SYNOPSIS
+    Builds service health alert objects from query results.
+
+.DESCRIPTION
+    The `Build-WAFServiceHealthObject` function processes the results obtained from the Azure Resource Graph query and constructs custom objects representing service health alerts with relevant details.
+
+.PARAMETER AdvQueryResult
+    The results from the Azure Resource Graph query.
+
+.INPUTS
+    System.Object[]. Accepts an array of query result objects.
+
+.OUTPUTS
+    System.Object[]. Returns an array of service health alert objects with detailed properties.
+
+.EXAMPLE
+    # Process query results to get service health alert objects
+    $queryResults = Invoke-WAFQuery -Query $Servicequery -SubscriptionIds $SubscriptionIds
+    $serviceHealthAlerts = Build-WAFServiceHealthObject -AdvQueryResult $queryResults
+
+.NOTES
+    Author: Kyle Poineal
+    Date: 2024-12-12
+#>
 function Build-WAFServiceHealthObject {
     param (
         [Parameter(Mandatory = $true)]
@@ -37,6 +90,62 @@ function Build-WAFServiceHealthObject {
     return $return
 }
 
+
+<#
+.SYNOPSIS
+    Represents a Service Health Alert retrieved from Azure.
+
+.DESCRIPTION
+    The `ServiceHealthAlert` class encapsulates the details of a service health alert from Azure. It provides properties to access alert information such as the name, subscription, status, event type, affected services, regions, and associated action groups. The class includes methods to parse and extract specific details from the alert data.
+
+.PROPERTIES
+    [string] Name
+        The name of the service health alert.
+
+    [string] Subscription
+        The name of the Azure subscription where the alert is configured.
+
+    [string] Enabled
+        Indicates whether the alert is enabled or disabled.
+
+    [string] EventType
+        The type of event the alert is configured to monitor (e.g., 'Service Health Incident', 'Planned Maintenance').
+
+    [string] Services
+        A comma-separated list of Azure services that the alert monitors.
+
+    [string] Regions
+        A comma-separated list of Azure regions that the alert monitors.
+
+    [string] ActionGroup
+        The name of the action group associated with the alert for notifications.
+
+.CONSTRUCTORS
+    ServiceHealthAlert([PSCustomObject] $Row)
+        Initializes a new instance of the `ServiceHealthAlert` class using the provided alert data.
+
+.METHODS
+    static [string] GetEventType([PSCustomObject] $Row)
+        Parses and returns the event type from the alert data.
+
+    static [string] GetServices([PSCustomObject] $Row)
+        Extracts and returns the services monitored by the alert.
+
+    static [string] GetRegions([PSCustomObject] $Row)
+        Extracts and returns the regions monitored by the alert.
+
+    static [string] GetActionGroupName([PSCustomObject] $Row)
+        Retrieves and returns the name of the action group associated with the alert.
+
+.EXAMPLE
+    # Example of creating ServiceHealthAlert objects from query results
+    $queryResults = Invoke-WAFQuery -Query $ServiceQuery -SubscriptionIds $SubscriptionIds
+    $serviceHealthAlerts = $queryResults.ForEach({ [ServiceHealthAlert]::new($_) })
+
+.NOTES
+    Author: Kyle Poineal
+    Date: 2024-12-12
+#>
 class ServiceHealthAlert {
     [string] $Name
     [string] $Subscription
