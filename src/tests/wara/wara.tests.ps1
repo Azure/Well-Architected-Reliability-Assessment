65b74d2972008369684e7c5603fe675f294de84d
Takeshi Katano <takatano@outlook.com>
2025-01-06 08:56:55 +0900

    Add test

--- src/tests/wara/wara.tests.ps1
+++ src/tests/wara/wara.tests.ps1
@@ -4,7 +4,12 @@ BeforeAll {
 }
 
 Describe 'Start-WARACollector' {
-
+    Context 'When given the Default parameter set without SubscriptionIds and ResourceGroups' {
+        It 'Should throw an exception with the specified message' {
+            $scriptBlock = { Start-WARACollector -TenantID '11111111-1111-1111-1111-111111111111' }
+            $scriptBlock | Should -Throw -ExpectedMessage 'The parameter SubscriptionIds or ResourceGroups is required when using the Default parameter set.'
+        }
+    }
 }
 
 Describe 'Build-ImpactedResourceObj' {
