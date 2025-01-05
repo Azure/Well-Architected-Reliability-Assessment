5c6aae1e6c3d9fb482a11aef2255de11f71e2889
Takeshi Katano <takatano@outlook.com>
2025-01-05 23:50:41 +0900

    Add test file for wara module

---
+++ src/tests/wara/wara.tests.ps1
@@ -0,0 +1,28 @@
+BeforeAll {
+    $modulePath = "$PSScriptRoot/../../modules/wara/wara.psd1"
+    Import-Module -Name $modulePath -Force
+}
+
+Describe 'Start-WARACollector' {
+
+}
+
+Describe 'Build-ImpactedResourceObj' {
+
+}
+
+Describe 'Build-ValidationResourceObj' {
+
+}
+
+Describe 'Build-ResourceTypeObj' {
+
+}
+
+Describe 'Build-SpecializedResourceObj' {
+
+}
+
+Describe 'Get-WARAOtherRecommendations' {
+
+}
