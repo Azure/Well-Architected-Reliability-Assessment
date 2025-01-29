64a2bd8787e14b8b32ec0c7d8b1268ef25402ae2
Claudio Merola <clvieira@microsoft.com>
2025-01-29 16:09:38 -0300

    New Assessment Findings File

--- src/modules/wara/reports/reports.psm1
+++ src/modules/wara/reports/reports.psm1
@@ -8,29 +8,23 @@ The function `Start-WARAReport` processes the Excel file created by the `Start-W
 .PARAMETER Help
 Switch to display help information.
 
-.PARAMETER Debugging
-Switch to enable debugging mode.
-
 .PARAMETER CustomerName
 Name of the customer for whom the report is being generated.
 
 .PARAMETER WorkloadName
 Name of the workload being assessed.
 
-.PARAMETER ExcelFile
-Path to the Excel file created by the "2_wara_data_analyzer" script.
+.PARAMETER includeLow
+Option to also consider Low Impact recommendations.
 
-.PARAMETER Heavy
-Switch to enable heavy processing mode. When enabled, this mode introduces additional delays using Start-Sleep at various points in the script to handle heavy environments more gracefully. This can help in scenarios where the system resources are limited or the operations being performed are resource-intensive, ensuring the script doesn't overwhelm the system.
+.PARAMETER ExpertAnalysisFile
+Path to the Excel file created by the "2_wara_data_analyzer" script.
 
 .PARAMETER PPTTemplateFile
 Path to the PowerPoint template file.
 
-.PARAMETER WordTemplateFile
-Path to the Word template file.
-
 .EXAMPLE
-Start-WARAReport -ExcelFile 'C:\WARA_Script\WARA Action Plan 2024-03-07_16_06.xlsx' -CustomerName 'ABC Customer' -WorkloadName 'SAP On Azure' -Heavy -PPTTemplateFile 'C:\Templates\Template.pptx' -WordTemplateFile 'C:\Templates\Template.docx'
+Start-WARAReport -ExpertAnalysisFile 'C:\WARA_Script\WARA Action Plan 2024-03-07_16_06.xlsx' -CustomerName 'ABC Customer' -WorkloadName 'SAP On Azure' -Heavy -PPTTemplateFile 'C:\Templates\Template.pptx'
 
 .LINK
 https://github.com/Azure/Azure-Proactive-Resiliency-Library-v2
@@ -39,17 +33,14 @@ function Start-WARAReport {
     [CmdletBinding()]
     Param(
     [switch] $Help,
-    #[switch] $GenerateCSV,
-    #[switch] $includeLow,
-    #[switch] $byPassValidationStatus,
-    [switch] $Debugging,
+    #[switch] $csvExport,
+    [switch] $includeLow,
     [string] $CustomerName,
     [string] $WorkloadName,
     [Parameter(mandatory = $true)]
-    [string] $ExcelFile,
-    [switch] $Heavy,
-    [string] $PPTTemplateFile,
-    [string] $WordTemplateFile
+    [string] $ExpertAnalysisFile,
+    [string] $AssessmentFindingsFile,
+    [string] $PPTTemplateFile
     )
 
     Write-host "Checking Version.." -ForegroundColor Cyan
