3b33f4865e3656d92fa64bc2b1208c9c2ef615d5
Casey Watson <cawatson@microsoft.com>
2025-02-04 13:08:08 -0600

    Working on runbook module

--- src/modules/wara/runbook/runbook.psm1
+++ src/modules/wara/runbook/runbook.psm1
@@ -1,55 +1,125 @@
-<#
-.SYNOPSIS
-    WARA Runbook module.
+using module ../utils/utils.psd1
 
-.DESCRIPTION
-    Enables developers to consume WARA runbook files.
-#>
+class runbookCheck {
+    [string]    $GroupingName
+    [string]    $SelectorName
+    [hashtable] $Parameters
+    [string[]]  $Tags
+}
 
-function Read-Runbook {
+class runbookCheckSet {
+    [hashtable] $Checks
+}
+
+class runbook {
+    [string[]]  $QueryPaths
+    [hashtable] $Parameters
+    [hashtable] $Variables
+    [hashtable] $Selectors
+    [hashtable] $Groupings
+    [hashtable] $CheckSets
+}
+
+function Read-RunbookFile {
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [string] $RunbookPath
+        [ValidateScript({ Test-FileExists $_ })]
+        [string] $Path
     )
 
-    # First, check to make sure the runbook actually exists...
-    if (!(Test-Path $RunbookPath -PathType Leaf)) {
-        # If not, fail early.
-        Write-Error "[-RunbookPath]: No runbook found at [$RunbookPath]."
-        $null
+    Test-RunbookFile -Path $Path
+
+    $runbook = [runbook]::new()
+    $runbookContent = Get-Content -Path $Path -Raw | ConvertFrom-Json -AsHashtable
+
+    $runbook.QueryPaths = ($runbookFile.query_paths ?? $runbookFile.query_overrides ?? @())
+    $runbook.Parameters = ($runbookFile.parameters ?? @{})
+    $runbook.Variables = ($runbookFile.variables ?? @{})
+    $runbook.Selectors = ($runbookFile.selectors ?? @{})
+    $runbook.Groupings = ($runbookFile.groupings ?? @{})
+    $runbook.CheckSets = @{}
+
+    foreach ($checkSetKey in $runbookContent.checks.Keys) {
+        $checkSet = [runbookCheckSet]::new()
+        $checkSetContent = $runbookContent.checks[$checkSetKey]
+
+        foreach ($checkKey in $checkSetContent.Keys) {
+            $check = [runbookCheck]::new()
+            $checkContent = $checkSetContent[$checkKey]
+
+            switch ($checkContent.GetType().Name.ToLower()) {
+                "string" {
+                    $check.GroupingName = $null
+                    $check.SelectorName = $checkContent
+                    $check.Parameters = @{}
+                    $check.Tags = @()
+                }
+                "orderedhashtable" {
+                    $check.GroupingName = $checkContent.grouping
+                    $check.SelectorName = $checkContent.selector
+                    $check.Parameters = ($checkContent.parameters ?? @{})
+                    $check.Tags = ($checkContent.tags ?? @())
+                }
+            }
+        }
+
+        $runbook.CheckSets[$checkSetKey] = $checkSet
+    } 
+}
+
+function Test-RunbookFile {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-FileExists $_ })]
+        [string] $Path
+    )
+
+    $errors = @()
+    $runbookJson = Get-Content -Path $Path -Raw | ConvertFrom-Json -AsHashtable
+
+    if ($runbookJson.ContainsKey("query_overrides") -and $runbookJson.ContainsKey("query_paths")) {
+        $errors += "-> [query_paths (query_overrides)]: Runbooks may have a [query_overrides] or [query_paths] section, but not both."
+    }
+
+    foreach ($queryPath in ($runbookJson.query_paths ?? $runbookJson.query_overrides ?? @())) {
+        if (-not (Test-Path -Path $queryPath -PathType Container)) {
+            $errors += "-> [query_paths (query_overrides)]: Folder [$queryPath] not found."
+        }
+    }
+
+    if (($runbookJson.selectors ?? @()).Count -eq 0) {
+        $errors += "-> [selectors]: At least one (1) selector must be defined."
+    }
+
+    if (($runbookJson.checks ?? @()).Count -eq 0) {
+        $errors += "-> [checks]: At least one (1) check set must be defined."
     }
     else {
-        # If so, let's read this runbook!
-        $runbook = @{
-            Parameters     = @{}
-            Selectors      = @{}
-            Checks         = @{}
-            QueryOverrides = @()
-        }
-        
-        # Read the runbook JSON
-        $runbookJson = Get-Content -Raw $RunbookPath | ConvertFrom-Json
+        foreach ($checkSetName in $runbookJson.checks.Keys) {
+            $checkSet = $runbookJson.checks[$checkSetName]
 
-        # Try to load parameters
-        $runbookJson.parameters.PSObject.Properties | ForEach-Object {
-            $runbook.Parameters[$_.Name] = $_.Value
-        }
+            if ($checkSet.Count -eq 0) {
+                $errors += "-> [checks]: Check set [$checkSetName] must define at least one (1) check."
+            }
 
-        # Try to load selectors
-        $runbookJson.selectors.PSObject.Properties | ForEach-Object {
-            $runbook.Selectors[$_.Name.ToLower()] = $_.Value
+            foreach ($checkName in $checkSet.Keys) {
+                $check = $checkSet[$checkName]  
+                
+                if ($check.GetType().Name.ToLower() -eq 'orderedhashtable') {
+                    if (-not $check.ContainsKey("selector")) {
+                        $errors += "-> [checks]: Check set [$checkSetName] check [$checkName] must define a [selector]."
+                    }
+                }
+            }
         }
-
-        # Try to load checks
-        $runbookJson.checks.PSObject.Properties | ForEach-Object {
-            $runbook.Checks[$_.Name.ToLower()] = $_.Value
-        }
-
-        # Try to load query overrides
-        $runbookJson.query_overrides | ForEach-Object {
-            $runbook.QueryOverrides += [string]$_
-        }
-
-        return [PSCustomObject]$runbook
     }
+
+    if ($errors.Count -gt 0) {
+        throw "Runbook [$Path] is invalid:`n$($errors -join "`n")"
+    }
+    
+    return $true
 }
+
