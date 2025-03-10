e67268f2a560e0ed1cd5fe6dad0f5ddd62d01ce1
clvieira_microsoft <Claudio.Merola@microsoft.com>
2025-03-10 19:54:24 -0300

    Charts v1

--- src/modules/wara/reports/3_wara_reports_generator.ps1
+++ src/modules/wara/reports/3_wara_reports_generator.ps1
@@ -1285,6 +1285,61 @@ $TableStyle = 'Light19'
 
   }
 
+  function Build-ExcelPivotChart {
+    Param($NewAssessmentFindingsFile)
+
+    $ExcelApplication = New-Object -ComObject Excel.Application
+    Start-Sleep -Seconds 2
+    $Ex = $ExcelApplication.Workbooks.Open($NewAssessmentFindingsFile)
+    Start-Sleep -Seconds 5
+    $Ex.Save()
+    $Ex.Close()
+    $ExcelApplication.Quit()
+    Get-Process -Name 'excel' -ErrorAction Ignore | Where-Object { $_.CommandLine -like '*/automation*' } | Stop-Process
+
+    $Excel1 = New-Object -TypeName OfficeOpenXml.ExcelPackage $NewAssessmentFindingsFile
+
+    $WS0 = $Excel1.Workbook.Worksheets | Where-Object { $_.Name -eq '1.Dashboard' }
+    $WS1 = $Excel1.Workbook.Worksheets | Where-Object { $_.Name -eq '7.PivotTable' }
+
+    $ChartP0 = $WS0.Drawings.AddChart('ChartP0', 'BarClustered', $WS1.PivotTables['P0'])
+    $ChartP0.SetSize(480, 700)
+    $ChartP0.SetPosition(18, 10, 2, 60)
+    $ChartP0.RoundedCorners = $false
+    #$ChartP0.DisplayBlanksAs = 'Gap'
+    $ChartP0.Legend.Font.LatinFont = '+mn-lt'
+    $ChartP0.Legend.Font.ComplexFont = '+mn-cs'
+    $ChartP0.Legend.Font.Size = 10
+    $ChartP0.YAxis.Font.LatinFont = '+mn-lt'
+    $ChartP0.YAxis.Font.ComplexFont = '+mn-cs'
+    $ChartP0.YAxis.Font.Size = 9
+    $ChartP0.XAxis.Font.LatinFont = '+mn-lt'
+    $ChartP0.XAxis.Font.ComplexFont = '+mn-cs'
+    $ChartP0.XAxis.Font.Size = 9
+    #$ChartP0.Style = 'Style7'
+    #$ChartP0.DataLabel.ShowValue = $true
+
+    $ChartP1 = $WS0.Drawings.AddChart('ChartP1', 'BarClustered', $WS1.PivotTables['P1'])
+    #$ChartP0.SetPosition(365, 195)
+    $ChartP1.SetSize(350, 700)
+    $ChartP1.SetPosition(18, 10, 6, 75)
+    $ChartP1.RoundedCorners = $false
+    #$ChartP1.DisplayBlanksAs = 'Gap'
+    $ChartP1.Legend.Font.LatinFont = '+mn-lt'
+    $ChartP1.Legend.Font.ComplexFont = '+mn-cs'
+    $ChartP1.Legend.Font.Size = 10
+    $ChartP1.YAxis.Font.LatinFont = '+mn-lt'
+    $ChartP1.YAxis.Font.ComplexFont = '+mn-cs'
+    $ChartP1.YAxis.Font.Size = 9
+    $ChartP1.XAxis.Font.LatinFont = '+mn-lt'
+    $ChartP1.XAxis.Font.ComplexFont = '+mn-cs'
+    $ChartP1.XAxis.Font.Size = 9
+    #$ChartP0.DataLabel.ShowValue = $true
+
+    $Excel1.Save()
+    $Excel1.Dispose()
+  }
+
   # Start the stopwatch to time the script
   $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
@@ -1355,7 +1410,6 @@ $TableStyle = 'Light19'
 
   $ResourcesTypes = $TempImpactedResources | Group-Object -Property 'Resource Type' | Sort-Object -Property 'Count' -Descending | Select-Object -First 10
 
-
   Write-Debug ((get-date -Format 'yyyy-MM-dd HH:mm:ss') + ' - Starting PowerPoint..')
   #Openning PPT
   $Application = New-Object -ComObject PowerPoint.Application
@@ -1389,6 +1443,8 @@ $TableStyle = 'Light19'
   Export-ExcelWorkloadInventory -ExcelWorkloadInventory $ExcelWorkloadInventory
 
   Build-ExcelPivotTable -NewAssessmentFindingsFile $NewAssessmentFindingsFile
+
+  Build-ExcelPivotChart -NewAssessmentFindingsFile $NewAssessmentFindingsFile
 
   if($csvExport.IsPresent)
     {
